9f9ab66ce80ae90dada26e31c0d29b04fd6405ea
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-04-16 21:55:24 +0800

    Update GradeScope.py

--- GradeScope.py
+++ GradeScope.py
@@ -6,4 +6,4 @@ st.info('This app calculates your pre-final grade. Please input information')
 
 components = st.text_input("What are the components of your grade?")
 
-st.write(components)
+#st.write(components)
